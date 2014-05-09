f641d9c895421ace7afd5675758211aa126cb8dc
csgroup <csstaff@CSs-MacBook-Pro.local>
2014-05-08 22:28:16 -0400

    Done with Pi.hs and theory!

--- A6/handout/Pi.hs
+++ A6/handout/Pi.hs
@@ -114,13 +114,52 @@ printer s = Embed (\_ -> putStr $ s ++ "\n") Nil
 type Gamma = M.Map Name Typ
 
 typeExp :: Gamma -> Exp -> Either String Typ
-typeExp = undefined
+typeExp g (EVar e) = case (g M.! e) of
+                         (TChan t) -> Right (TChan t)
+                         (TTup t)  -> Right (TTup t)
+typeExp g (ETup e) = let type_list = L.map (\x -> case (typeExp g x) of
+                                                  Right (TChan t) -> (TChan t)
+                                                  Right (TTup t)  -> (TTup t) ) e in Right (TTup type_list)
 
 typePat :: Gamma -> Pattern -> Typ -> Either String Gamma
-typePat = undefined
+typePat g (PVar p) t         = Right (M.insert p t (M.empty))
+typePat g (PTup p) (TChan t) = Left "Pattern did not type check" 
+typePat g (PTup p) (TTup t)  = let tup_list = (L.zip p t) in
+                               let tup_map = L.foldr (\(pat,typ) temp_map -> case (typePat g pat typ) of
+                                                                                 Right gam -> M.union gam temp_map) M.empty tup_list in Right tup_map
+typePat g Wild     t         = Right M.empty
 
 checkPi :: Gamma -> Pi -> Either String ()
-checkPi = undefined
+checkPi g Nil               = Right ()
+checkPi g (p1 :|: p2)       = case (checkPi g p1, checkPi g p2) of
+                            (Right (), Right ()) -> Right ()
+                            _                    -> Left "Concurrent process did not type check"
+checkPi g (New n t p)       = case (checkPi g p) of
+                                  Right () -> if ( M.member n g == True ) 
+                                                 then Left "New process did not type check"
+                                              else  Right ()
+                                  _        -> Left "New process did not type check"
+checkPi g (Out n e)         = case (g M.! n) of
+                                  (TChan c) -> case (typeExp g e) of
+                                               Right typ -> if (typ == c) then Right () else Left "Out process did not type check"
+                                               _         -> Left "Out process did not type check"
+                                  _         -> Left "Out process did not type check"
+checkPi g (Inp n pat p)     = case (g M.! n) of
+                                  (TChan c) -> case (typePat g pat c) of 
+                                                   Right gam -> case checkPi (M.union g gam) p of
+                                                                    Right () -> Right ()
+                                                                    _        -> Left "Imp process did not type check"
+                                                   _         -> Left "Inp process did not type check"
+                                  _         -> Left "Inp process did not type check"
+
+
+checkPi g (RepInp n pat p)  = case (checkPi g (Inp n pat p)) of
+                                  Right () -> Right ()
+                                  _        -> Left "RepInp rocess did not type check"
+checkPi g (Embed (envio) p) = checkPi g p
+
+
+
 
 check :: Pi -> Either String ()
 check p = checkPi M.empty p
@@ -140,7 +179,16 @@ type Env = M.Map Name Value
 -- eval_p env p v 
 -- match a value v against a pattern p and extend environment env
 eval_p :: Env -> Pattern -> Value -> Env
-eval_p = undefined
+eval_p e (PVar n) (VChan c) = M.insert n (VChan c) e
+eval_p e (PVar n) (VTup v) = M.insert n (VTup v) e
+eval_p e (PTup n) (VChan c) = e
+eval_p e (PTup n) (VTup v) = if (L.length n /= L.length v)
+                                 then e
+                             else if (L.length n == 0)
+                                 then e
+                             else let z = L.zip n v in L.foldr (\(x1,x2) y -> eval_p e x1 x2) e z
+eval_p e (Wild) (VChan c) = e
+eval_p e (Wild) (VTup v) = e
 
 -- eval_e env e
 -- evaluates e to a value in environment env
@@ -152,7 +200,20 @@ eval_e env (ETup es) = VTup (eval_es env es)
     eval_es env (e:es) = eval_e env e : eval_es env es
 
 run :: Env -> Pi -> IO ()
-run = undefined
+run e Nil               = putStr "done\n"
+run e (p1 :|: p2)       = parallel [run e p1, run e p2]
+run e (New n t p)       = do
+  c <- newChan
+  run (M.insert n (VChan c) e) p
+run e (Out n exp)       = case (e M.! n) of
+                            (VChan c) -> writeChan c (eval_e e exp)
+run e (Inp n pat p)     = do
+  v <- readChan (case e M.! n of (VChan c) -> c)
+  run (eval_p e pat v) p
+run e (RepInp n pat p)  = do
+  v <- readChan (case e M.! n of (VChan c) -> c)
+  parallel [run (M.insert n v e) p, run e (RepInp n pat p)]
+run e (Embed (envio) p) = run e p
 
 start :: Pi -> IO ()
 start p = run M.empty p
